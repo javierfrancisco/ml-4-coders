a42996fc55b139bce937803dfca336d552b3d0f0
Javier Alvarez <zenkiu@javiers-mini.myfiosgateway.com>
2023-02-25 14:52:55 -0500

    Add hello world machine learning

---
+++ main.py
@@ -0,0 +1,22 @@
+import tensorflow as tf
+import numpy as np
+from tensorflow.keras import Sequential
+from tensorflow.keras.layers import Dense
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+    model = Sequential([Dense(units=1, input_shape=[1])])
+    model.compile(optimizer='sgd', loss='mean_squared_error')
+    xs = np.array([-1.0, 0.0, 1.0, 2.0, 3.0, 4.0], dtype=float)
+    ys = np.array([-3.0, -1.0, 1.0, 3.0, 5.0, 7.0], dtype=float)
+    model.fit(xs, ys, epochs=5000)
+    print(model.predict([10.0]))
+    #18.976208
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
